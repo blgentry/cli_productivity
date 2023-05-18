ad9ddfc71659f41c9863241020cf95c4f24a7617
Brian Gentry <bgentry@admin.rcc.fsu.edu>
2023-05-18 13:56:28 -0400

    better setup for students to start with

--- findnumbers.pl
+++ findnumbers.pl
@@ -3,11 +3,13 @@
 $file = $ARGV[0];
 open(F,$file);
 while(<F>) {
-    #    if (@matches = $_ =~ /(\d+)/g) {
+    #if (@matches = $_ =~ /(\d+)/g) {
     if ($_ =~ /(\d+)/g) {
         print "$1 ";
+        #print "@matches\n";
         #print join(" ", @matches) . "\n";
         #push(@allnumbers, @matches);
     }
 }
+#print "\nAll numbers:\n\n";
 #print join("\n", @allnumbers);
